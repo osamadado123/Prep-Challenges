434ad2c27ff04a9f88a6a3eb880a0825ec280f73
osamadado123 <osama.aldado@gmail.com>
2022-08-01 09:39:43 +0300

    exercise01 git commit -m exercise01

---
+++ code-challenges/challenges-01.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// --------------------------------------------------------------------------------------------------------
+//  Challenge 01:
+//  Required:
+//
+//  Write a function that takes an array and returns the maximum value in the provided array.
+//  Without using build-in methods.
+//
+//  Input: [12, 32, 22, 45, 78, 12, 50]
+//  Output: 78
+
+const findMax = (arr) => {
+  let max=0;
+  for(let i= 0;i<arr.length;i++){
+    if(max <arr[i])
+      max = arr[i];
+   
+  }
+  return max;
+}
+// --------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------
+//  Challenge 02:
+//  Required:
+//
+//  Write a function that takes an array and returns the sum of the numeric values.
+//  Inside the array (there could be strings), without using built-in methods (You can use `typeof`)
+//
+//  Input: [20, '234', 'car', 41, 20, 'chair']
+//  Output: 81
+const sumNums = (arr) => {
+  let sum=0;
+  var int= "number";
+  for( let i=0;i<arr.length;i++){
+  if (typeof arr[i] == int)
+  sum= arr[i]+ sum;
+  else 
+  continue;
+  }
+  return sum;
+};
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = { findMax, sumNums };
